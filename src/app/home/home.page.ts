b8c075109093587cd0acc9fa33baf5d41789b080
creador <isidrotorresadriandavid@gmail.com>
2024-01-22 16:25:40 -0500

    modificaciones del dia viernes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slides =[
+  {
+    title: "titulo 1",
+    descripcion:"",
+    image: "",
+    helptext: "",
+    class: ""
+  },
+  {
+    title: "",
+    descripcion:"descripcion 2",
+    image: "",
+    helptext: "",
+    class:""
+  },
+  {
+    title: "",
+    descripcion:"",
+    image: "",
+    helptext: "texto de ayuda 3",
+    class:""
+  }
+  ]
+
   constructor() {}
 
 }
